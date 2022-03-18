4eef7fed3aa2cbff4308a893fe37e0b07eabf5d5
Chunyounghawn <zing0982@naver.com>
2022-03-18 14:36:53 +0900

    add_weather

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,23 @@
-function onGeoSuccess(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live in", lat, lng);
+const API_KEY = "222f96af65dd1d2dc12fc173376f3243"
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude
+  const lon = position.coords.longitude
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child ")
+      const city = document.querySelector("#weather span:last-child ")
+      console.log(data)
+      city.innerText = data.name
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+    })
 }
 
 function onGeoFailed() {
-    alert("Can't find you.")
+  alert("Can't find you.")
 }
 
-
-navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoFailed);
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoFailed)
 onGeoSuccess
